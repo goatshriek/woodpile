647a5cff0ae2441280b4c3134d6e448fe6277d7b
Joel <stumpless@verizon.net>
2015-11-28 17:09:53 -0500

    add allocation validation option for autotools build

--- tool/autotools/autogen.sh
+++ tool/autotools/autogen.sh
@@ -3,6 +3,6 @@
 autoreconf \
 && mkdir build \
 && cd build \
-&& ../configure --enable-parameter-validation \
+&& ../configure --enable-parameter-validation --enable-allocation-validation \
 && make \
 && make check
