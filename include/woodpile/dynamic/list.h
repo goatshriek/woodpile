8fc03efa1e0220b7d76883ced30d4bcd3df1010b
goatshriek <stumpless@verizon.net>
2016-05-21 11:57:36 -0400

    rename DynamicList to DList in documentation

--- include/woodpile/dynamic/list.h
+++ include/woodpile/dynamic/list.h
@@ -3,14 +3,14 @@
 
 /**
  * @file
- * DynamicList declaration and functions
+ * DList declaration and functions
  */
 
 /**
- * @struct DynamicList
- * The DynamicList data structure is a simple doubly linked list implementation.
+ * @struct dlist_t
+ * The DList data structure is a simple doubly linked list implementation.
  *
- * The DynamicList is optimized for insertions and deletions at either end, and
+ * The DList is optimized for insertions and deletions at either end, and
  * sequential walks via the iterators. If random access is needed then the
  * StaticList structure is more suitable.
  *
@@ -27,10 +27,10 @@ struct dlist_t;
 typedef struct dlist_t dlist_t;
 
 /**
- * Adds a single element to the end of a DynamicList. If element is NULL then
+ * Adds a single element to the end of a DList. If element is NULL then
  * nothing is done.
  *
- * @param list the DynamicList to append to. Must not be NULL.
+ * @param list the DList to append to. Must not be NULL.
  * @param element the element to append
  *
  * @return list
@@ -40,12 +40,12 @@ DListAppend
 ( dlist_t *list, void *element );
 
 /**
- * Appends all elements in the second DynamicList to the end of the first. The
+ * Appends all elements in the second DList to the end of the first. The
  * second list is not changed as a result of the operation. The elements are
- * appended in the order they exist in the second DynamicList.
+ * appended in the order they exist in the second DList.
  *
- * @param first the DynamicList to append to. Must not be NULL.
- * @param second the DynamicList to append
+ * @param first the DList to append to. Must not be NULL.
+ * @param second the DList to append
  *
  * @return first
  */
@@ -54,9 +54,9 @@ DListAppendAll
 ( dlist_t *first, const dlist_t *second );
 
 /**
- * Gets the last element in the DynamicList.
+ * Gets the last element in the DList.
  *
- * @param list the DynamicList to get the element from. Must not be NULL.
+ * @param list the DList to get the element from. Must not be NULL.
  *
  * @return the last element of list, or NULL if list is empty
  */
@@ -65,13 +65,13 @@ DListBack
 ( const dlist_t *list );
 
 /**
- * Searches a DynamicList for an element.
+ * Searches a DList for an element.
  *
  * Note that the element is identified using a pointer; if two structures are
  * considered equal but are located at different addresses then they are not
  * considered equal by this function.
  *
- * @param list the DynamicList to search
+ * @param list the DList to search
  * @param element the element to search for
  *
  * @return the number of entries of element in list
@@ -81,32 +81,32 @@ DListContains
 ( const dlist_t *list, const void *element );
 
 /**
- * Creates a copy of a DynamicList. Elements within the DynamicList are not
- * copied, meaning that changes made to elements in the original DynamicList
+ * Creates a copy of a DList. Elements within the DList are not
+ * copied, meaning that changes made to elements in the original DList
  * will also change the elements in the copy. Changes made to the original
- * DynamicList will not affect the copy.
+ * DList will not affect the copy.
  *
- * @param list the DynamicList to copy. Must not be NULL.
+ * @param list the DList to copy. Must not be NULL.
  * 
- * @return a copy of the original DynamicList or NULL on failure
+ * @return a copy of the original DList or NULL on failure
  */
 dlist_t *
 DListCopy
 ( const dlist_t *list );
 
 /**
- * Destroys a DynamicList. Does not affect the elements stored within.
+ * Destroys a DList. Does not affect the elements stored within.
  *
- * @param list the DynamicList to destroy
+ * @param list the DList to destroy
  */
 void
 DListDestroy
 ( const dlist_t *list );
 
 /**
- * Gets the first element in the DynamicList.
+ * Gets the first element in the DList.
  *
- * @param list the DynamicList to get the element from. Must not be NULL.
+ * @param list the DList to get the element from. Must not be NULL.
  *
  * @return the first element of list, or NULL if list is empty
  */
@@ -115,8 +115,8 @@ DListFront
 ( const dlist_t *list );
 
 /**
- * Gets the element at the specified position in the DynamicList. If the
- * DynamicList does not have an element at this index then NULL is returned.
+ * Gets the element at the specified position in the DList. If the
+ * DList does not have an element at this index then NULL is returned.
  * NULL is returned if list is NULL or empty.
  *
  * Negative indexes count backwards through the list, for example an index of
@@ -127,10 +127,10 @@ DListFront
  * return the second element in list. Likewise, an index of -4 will return the
  * last element in list.
  *
- * @param list the DynamicList to get the element from
+ * @param list the DList to get the element from
  * @param index the index of the element to retrieve
  *
- * @return the element of the DynamicList at the given index, or NULL if it does not
+ * @return the element of the DList at the given index, or NULL if it does not
  * exist
  */
 void *
@@ -138,31 +138,31 @@ DListGet
 ( const dlist_t *list, int index );
 
 /**
- * Checks whether or not a DynamicList is empty. A NULL DynamicList is
+ * Checks whether or not a DList is empty. A NULL DList is
  * considered empty.
  *
- * @param list the DynamicList to check
+ * @param list the DList to check
  *
- * @return a positive value if the DynamicList is empty, 0 otherwise
+ * @return a positive value if the DList is empty, 0 otherwise
  */
 unsigned short
 DListIsEmpty
 ( const dlist_t *list );
 
 /**
- * Creates a new DynamicList.
+ * Creates a new DList.
  *
- * @return a new DynamicList or NULL on failure
+ * @return a new DList or NULL on failure
  */
 dlist_t *
 DListNew
 ( void );
 
 /**
- * Adds a single element to the beginning of a DynamicList. If element is NULL
+ * Adds a single element to the beginning of a DList. If element is NULL
  * then nothing is done.
  *
- * @param list the DynamicList to prepend to. Must not be NULL.
+ * @param list the DList to prepend to. Must not be NULL.
  * @param element the element to prepend
  *
  * @return list
@@ -172,19 +172,19 @@ DListPrepend
 ( dlist_t *list, void *element );
 
 /**
- * Gets the number of elements in the DynamicList. An empty DynamicList will
+ * Gets the number of elements in the DList. An empty DList will
  * return 0.
  *
- * @param list the DynamicList to get the size of
+ * @param list the DList to get the size of
  *
- * @return the number of elements in the DynamicList
+ * @return the number of elements in the DList
  */
 unsigned
 DListSize
 ( const dlist_t *list );
 
 /**
- * Creates a string representation of the given DynamicList, using the provided
+ * Creates a string representation of the given DList, using the provided
  * function to get the string representation of each element. If the
  * element_to_string function is NULL, then the element is printed as a void
  * pointer.
@@ -198,11 +198,11 @@ DListSize
  * string literals, then the function must return a copy of the string rather
  * than the string itself.
  *
- * @param list the DynamicList to get a representation of. Must not be NULL.
+ * @param list the DList to get a representation of. Must not be NULL.
  * @param element_to_string a function returning string representations of
  * elements
  *
- * @return a char buffer holding a string representation of the DynamicList
+ * @return a char buffer holding a string representation of the DList
  */
 char *
 DListToString
